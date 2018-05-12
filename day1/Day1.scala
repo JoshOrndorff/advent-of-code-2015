ddeee76247b73eda3afaa7bc7c07b3935b30ee87
Josh Orndorff <admin@joshorndorff.com>
2018-05-11 20:08:28 -0400

    Solved both parts of day 1!

--- day1/Day1.scala
+++ day1/Day1.scala
@@ -1,20 +1,29 @@
-// not -7000
-//not -280
 
 object Day1 {
 
   val input = io.Source.fromFile("input.txt")
   val instructions = try input.mkString finally input.close()
 
-  def countFloors(inst: String): Int = {
-    def countRec(inst: String, acc: Int):Int = {
-      if (inst == "") acc
-      else if (inst.substring(0, 1) == "(") countRec(inst.substring(1), acc + 1)
-      else countRec(inst.substring(1), acc - 1)
-    }
-    countRec(inst, 0)
+  // Part 1
+  def endingFloor(inst: String, acc: Int): Int = {
+    if (inst == "") acc
+    else if (inst.substring(0, 1) == "(") endingFloor(inst.substring(1), acc + 1)
+    else endingFloor(inst.substring(1), acc - 1)
   }
 
-  def main(args: Array[String]) = println(countFloors(instructions))
+  // Part 2
+  def firstBasement(inst: String, acc: Int, pos: Int): Int = {
+    if (acc == -1) pos
+    else if (inst == "") -1 // Something went wrong. Instructions ended without hitting basement.
+    else if (inst.substring(0, 1) == "(") firstBasement(inst.substring(1), acc + 1, pos + 1)
+    else firstBasement(inst.substring(1), acc - 1, pos + 1)
+  }
+
+
+  // Print results
+  def main(args: Array[String]) = {
+    println("Part 1: Santa ends on floor: " + endingFloor(instructions, 0))
+    println("Part 2: Santa hits basement: " + firstBasement(instructions, 0, 0))
+  }
 
 }
