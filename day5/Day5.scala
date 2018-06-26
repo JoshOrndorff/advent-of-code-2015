dc38a1a1a38399a3c988589f7b891885ccd21ae4
Joshy Orndorff <admin@joshorndorff.com>
2018-06-26 19:34:32 -0400

    Solved Day5 part2.

--- day5/Day5.scala
+++ day5/Day5.scala
@@ -7,16 +7,24 @@ object Day5 extends App{
     finally input.close()
 
   // Run the tests
-  println(isNice("ugknbfddgicrmopn".toList))
-  println(isNice("aaa".toList))
-  println(isNice("jchzalrnumimnmhp".toList))
-  println(isNice("haegwjzuvuyypxyu".toList))
-  println(isNice("dvszwmarrgswjxmb".toList))
+  println(isNice1("ugknbfddgicrmopn".toList))
+  println(isNice1("aaa".toList))
+  println(isNice1("jchzalrnumimnmhp".toList))
+  println(isNice1("haegwjzuvuyypxyu".toList))
+  println(isNice1("dvszwmarrgswjxmb".toList))
+  println()
+  println(isNice2("qjhvhtzxzqqjkmpb".toList))
+  println(isNice2("xxyxx".toList))
+  println(isNice2("uurcxstgmygtbstg".toList))
+  println(isNice2("ieodomkazucvgmuy".toList))
+  println()
 
   // Print result
-  println(strs.map(isNice(_)).filter(x => x).size)
+  println(strs.filter(isNice1).size)
+  println(strs.filter(isNice2).size)
 
-  def isNice(s: List[Char]): Boolean =
+  // Part 1 Functions
+  def isNice1(s: List[Char]): Boolean =
     threeVowels(s) && doubles(s) && !forbidden(s)
 
   def threeVowels(s: List[Char]): Boolean = countVowels(0, s) >= 3
@@ -47,4 +55,27 @@ object Day5 extends App{
     case _ :: tail => forbidden(tail)
     case _ => false
   }
+
+  // Part 2 Functions
+  def isNice2(s: List[Char]): Boolean =
+    dupePair(s) && sandwich(s)
+
+  def dupePair(s: List[Char]): Boolean =
+    if (s.size < 4) false
+    else if (containsPair(s.drop(2), s.slice(0, 2))) true
+    else dupePair(s.tail)
+
+  def containsPair(haystack: List[Char], needle: List[Char]): Boolean =
+    if (haystack.size < 2) false
+    else if (haystack.slice(0, 2) == needle) true
+    else containsPair(haystack.tail, needle)
+
+  // @tailrec // Not sure why annotation isn't working
+  def sandwich(s: List[Char]): Boolean = s match {
+    case Nil => false
+    case _ :: Nil => false
+    case _ :: _ :: Nil => false
+    case a :: b :: c :: tail => if (a == c) true else sandwich(b :: c :: tail)
+  }
+
 }
